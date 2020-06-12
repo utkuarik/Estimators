ebadde1da6d98c198e4716e4c1670d2aa5878ccd
Utku <utkuarik@sabanciuniv.edu>
2020-06-12 17:50:48 +0300

    Readme file changed and refactoring

--- pi_estimation.py
+++ pi_estimation.py
@@ -10,13 +10,11 @@ import math
 
 ### Euclidean distance calculator 
 def dist_cal (x1, y1 ,x2, y2):
-    
     distance = math.sqrt((x1 - x2)**2 + (y1 -y2)**2)
-    
     return distance
+
 ### Random points generator
 def create_random(n_val):
-    
     point_list = []
     
     for x in range(n_val):
@@ -27,16 +25,12 @@ def create_random(n_val):
         point_list.append([x,y])
     return point_list
 
-
 inside = 0
 outside = 0
 n_val = int(input("\nEnter the number of points that will be generated: "))
-
-
 points = create_random(n_val)
 
 for i in points:
-    
     if dist_cal(i[0],i[1], 0,0) <= 10000:
         inside = inside +1
 
